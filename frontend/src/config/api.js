31376d8ae156320ef5ee6d224ca8d3e2708e73a3
SATYAM-TYAGI <142072557+SATYAM-TYAGI@users.noreply.github.com>
2026-06-29 20:02:36 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
 // backend api url - from .env or fallback to live server
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "https://popflix-lp68.onrender.com";
+  import.meta.env.VITE_API_URL || "https://pop-flix-five.vercel.app/";
